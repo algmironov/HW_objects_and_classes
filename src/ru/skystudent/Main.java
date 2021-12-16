4eb9d31a2e03b12a6d693ec4c7350d29df152e97
algmironov <al.g.mironov@yandex.ru>
2021-12-16 22:33:08 +0300

    Готовое Д/З Начальный уровень

--- src/ru/skystudent/Main.java
+++ src/ru/skystudent/Main.java
@@ -3,6 +3,14 @@ package ru.skystudent;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Author alexeyVasilev = new Author("Алексей", "Васильев");
+        Author kenKouzen = new Author("Кен", "Коузен");
+        Book javaForAll = new Book("Java для всех", alexeyVasilev, 2019);
+        Book modernJava = new Book("Современный Java", kenKouzen, 2019);
+        modernJava.setYearOfPublishing(2017);
+        System.out.println(javaForAll.getYearOfPublishing());
+        System.out.println(modernJava.getYearOfPublishing());
+        System.out.println(modernJava.getAuthor());
+        System.out.println(javaForAll.getName());
     }
 }
